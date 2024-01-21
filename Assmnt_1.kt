17886276dcb794e0ef6b4c033085899abc8b5823
OmarStark69 <omartinezh1996@gmail.com>
2024-01-21 05:31:23 -0600

    Task 1 Instead of doing a equivalence between roman symbols and numbers, my approach was to find a pattern to make this convertion, so that it will work on any number of symbols without making the direct conversion of each one to integers. The declared array of symbols will be useful to transform each digit of the number to its equivalent. The input number is read from right to left, to start with units, then the tens, hundreds and finally thousands. If it is the first digit (units), then the symbols involved to represent from 1-9 will be I,V and X, to represent second digit (tens): X,L,C, and so on, thats why the array of symbols is used, each digit position (i) is related to its value, and will indicate wich symbols to use. First, to convert from 1-3, module from 5 and then module from 4 is calculated (mod4), that variable will indicate how many "one" symbols (I,X,C,M) to add to the String "roman" while the digit is 1-3 or 6-8, the module operations helps avoiding printing this symbols while the digit is 4,5 or 9.

---
+++ Assmnt_1.kt
@@ -0,0 +1,23 @@
+fun int_to_rom(num: String){
+    val letters = arrayOf("I", "V", "X", "L", "C", "D", "M") // array of roman symbols
+    val max = 4*Math.pow(10.toDouble(),(letters.size/2).toDouble()).toInt() // max value of roman number
+    print("${num}\t---->\t")
+    val num = num.reversed()    // reverse number to start with the units
+    val roman = StringBuilder()
+    for (i in num.indices) {    // for each digit
+        var mod4 = (num[i].digitToInt()%5) % 4 // is a 1,2 or 3?
+        roman.insert(0,letters[2*i].repeat(mod4))   // add 1,2 or 3 symbols
+        }
+    println(roman)}
+
+fun main() {
+    var num = readLine()!!
+    while (num!=""){
+        int_to_rom(num)
+        num = readLine()!!
+    }
+    // test cases
+    // for (num in arrayOf("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15")){
+    //     int_to_rom(num)
+    // }
+}
