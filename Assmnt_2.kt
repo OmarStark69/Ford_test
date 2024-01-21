4a1506473ab9388cb35bc0a3bfd1323fe58ad58d
OmarStark69 <omartinezh1996@gmail.com>
2024-01-21 06:22:02 -0600

    For Part 2, a similar approach is used, using the position of each character in the string of roman symbols to relate it to an integer number. Again, an array of roman symbols is created, then each char of the string is read from left to right, so that the first symbols will have the greater value. When the symbol is read, it is first compared to the greatest value roman symbol (M), if the character matches, then its value will be added to the variable "num", this value is calculated as follow: val=10^{(pos+1)//2} That way, if the matching symbol is M, its position in the array will be 6, and val=1000, if is C, the position in the array is 4, and val=100, and so on. After the value is added to "num", the matching symbol is removed from the string i.e: from MMM to MM, and "num" now is 1000. This step is repeated until the current character doesnt match the current symbol in the array, when this happens, the variable "pos" is modified substracting 1, so that the next symbol in array with lower value is compared, now again when it matches the current character in string, the related value is added to "num" and so on. This will work with roman numbers with only 1-valued symbols, i.e: II, III, XXIII, CXXIII, MMXXCIII. The cases with 5 value symbols will be added next

---
+++ Assmnt_2.kt
@@ -0,0 +1,28 @@
+fun rom_to_dec(roman: String){
+    var roman = roman // copy input
+    print("${roman}\t---->\t") 
+    val letters = arrayOf("I","V", "X", "L", "C", "D", "M") // array of roman symbols
+    var num= 0 // initialize number
+    var pos = letters.size -1 // initialize position of symbol with max value
+    while (roman.length>0){
+       if (letters[pos]==roman[0].toString().uppercase()) { // if the first char of roman is the current symbol
+            num += Math.pow(10.toDouble(),((pos+1)/2).toDouble()).toInt()/(1+pos%2) // add value of symbol to number
+            roman = roman.drop(1) // remove first char of roman
+       }else{pos--} // if the first char of roman is not the current symbol, try with the next one with less value
+       if (pos==-1){pos = letters.size -1} // if the first char of roman is not any symbol, start again with the symbol with max value
+    }
+    println(num)
+}
+
+fun main() {
+    var roman = readLine()!! // read input
+    while (roman!=""){
+        rom_to_dec(roman)
+        roman = readLine()!!
+    }
+
+    // test cases
+    // for (roman in arrayOf("I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV")){
+    //     rom_to_dec(roman)
+    // }
+}
