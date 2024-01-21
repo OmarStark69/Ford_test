ccbb11238e69d0c27051252ef0bdb4dd7a958425
OmarStark69 <omartinezh1996@gmail.com>
2024-01-21 07:00:28 -0600

    Now, with 4 and 9 valued symbols (IV,IX,XL,XC,CD,CM), the process described in previous commit will cause the program to stop without removing each one of the characters in the string, and therefore, "num" will be wrong, i.e with "MMCMIII" the algorithm will stop at "MIII" since M has a greater value than C and in consequence, a greater position in symbol array, and returning num=2100, to adress this, the "pos" variable is reset to the greatest value symbol, and the last removed symbol position is stored in "last_pos" in this example, for C, last_pos=4. Now when repeating the process, The M in "MIII" will be removed, and 1000 added to num, so that num=3100, but since M position in "letters" array is 6, pos>last_pos, an IF sentence is added to make this comparison, so that to modify "num" the next expression is used: num=num- 10^{(pos-1)/2}*2 In the current example, with M, pos=6 and - 10^{(pos-1)/2}*2=-200. The reason to substract 200, instead of 100, is that previously 100 was added when matching C in "MMCMIII", this will add the 900 related to "CM", finally obtaining num=2903 when algorithm finishes.

--- Assmnt_2.kt
+++ Assmnt_2.kt
@@ -4,9 +4,14 @@ fun rom_to_dec(roman: String){
     val letters = arrayOf("I","V", "X", "L", "C", "D", "M") // array of roman symbols
     var num= 0 // initialize number
     var pos = letters.size -1 // initialize position of symbol with max value
+    var last_pos = pos  // initialize last position of last "consumed" char in roman
     while (roman.length>0){
        if (letters[pos]==roman[0].toString().uppercase()) { // if the first char of roman is the current symbol
-            num += Math.pow(10.toDouble(),((pos+1)/2).toDouble()).toInt()/(1 + pos%2) // add value of symbol to number
+            num += Math.pow(10.toDouble(),((pos+1)/2).toDouble()).toInt()/(1+pos%2) // add value of symbol to number
+            if (pos>last_pos){ // if the current symbol value is greater than the last one
+               num-= Math.pow(10.toDouble(),((pos-1)/2).toDouble()).toInt()*2 // subtract the value of the last symbol twice
+            }
+            last_pos = pos // update last position
             roman = roman.drop(1) // remove first char of roman
        }else{pos--} // if the first char of roman is not the current symbol, try with the next one with less value
        if (pos==-1){pos = letters.size -1} // if the first char of roman is not any symbol, start again with the symbol with max value
